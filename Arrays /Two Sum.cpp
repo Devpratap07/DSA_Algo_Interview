7d03329420fadb9f4aa1d851fa48dba1ad79e15b
Dev Pratap <devpratap324@gmail.com>
2025-07-26 22:08:52 +0530

    Two Sum.cpp

---
+++ Arrays /Two Sum.cpp
@@ -0,0 +1,36 @@
+// Approach - 1(brute force)
+// T.C -> o(n^2)
+// S.C -> o(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]== target){
+                    return {i,j};
+                } 
+            }
+        }
+        return {};  
+    }
+};
+
+// Approach- 2(by using map)
+// T.C -> o(n)
+// S.C -> o(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            int remain = target - nums[i];
+            if(mp.find(remain)!=mp.end()){
+                return {mp[remain],i}; // return map value(index) and current index
+            }
+            mp[nums[i]]=i; // key(number and value(index) pushing in map
+        }
+        return {}; // for empty case
+    }
+};
