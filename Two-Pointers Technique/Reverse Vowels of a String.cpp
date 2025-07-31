2b19ea2dd8d7409891a51752a2316ae23339a3a4
Dev Pratap <devpratap324@gmail.com>
2025-08-01 01:27:57 +0530

    Reverse Vowels of a String.cpp

---
+++ Two-Pointers Technique/Reverse Vowels of a String.cpp
@@ -0,0 +1,33 @@
+// Approach -> 1
+// T.C -> O(n)
+// S.C -> O(1)
+class Solution {
+public:
+bool isVowel(char &ch){
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+    string reverseVowels(string s) {
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(isVowel(s[i])!=true){
+                i++;
+            }
+            else if(isVowel(s[j])!=true){
+                j--;
+            }
+            else{
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
